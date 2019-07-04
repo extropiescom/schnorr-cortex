c83e96a74ec53cecf45bddea1031c660e5effcdf
chester <chester@pixelauth.com>
2019-07-04 14:46:27 +0800

    alloc ok

--- src/lib.rs
+++ src/lib.rs
@@ -282,6 +282,8 @@ extern crate serde;
 #[cfg(all(test, feature = "serde"))]
 extern crate bincode;
 
+extern crate cortex_m_rt as rt;
+
 #[macro_use]
 mod serdey;
 pub mod points;
@@ -328,6 +330,15 @@ pub struct sr_data{
     len: u32
 }
 
+pub fn test_box()-> *mut u8{
+	let start:usize = 0x2000968;
+    let size:usize = 0x1800; // in bytes
+	unsafe { ALLOCATOR.init(start, size) }
+	let keypair_bytes: [u8;96] = [74,83,195,251,188,89,151,14,229,248,90,248,19,135,93,255,193,58,144,74,46,83,174,126,101,250,13,234,110,98,201,1,159,7,231,190,85,81,56,122,152,186,151,124,115,45,8,13,203,15,41,160,72,227,101,105,18,198,83,62,50,238,122,237,156,102,163,57,200,52,79,146,47,195,32,108,181,218,232,20,165,148,192,23,125,211,35,92,37,77,156,64,154,101,184,8];
+	let b = Box::new(keypair_bytes);
+	return Box::into_raw(b) as *mut u8;
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn test_sign_verify()->u8
 {
@@ -346,8 +357,9 @@ pub unsafe extern "C" fn test_sign_verify()->u8
 
 #[no_mangle]
 pub unsafe extern "C" fn test_sign_ptr() -> *mut u8{
-	 let start:usize = 0x2000700;
-     let size:usize = 256; // in bytes
+	 //let start:usize = 0x2000700;
+	  let start = rt::heap_start() as usize;
+     let size:usize = 1024; // in bytes
 	 unsafe { ALLOCATOR.init(start, size) }
 
 	const SIGNING_CTX: &'static [u8] = b"good";
